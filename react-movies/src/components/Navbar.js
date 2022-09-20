0dd61b5d6322c329f683bc620007e363a7d0bb03
André <a.carvalho@runtime-revolution.com>
2022-09-20 15:22:00 +0100

    type(feature): code well structured

--- react-movies/src/components/Navbar.js
+++ react-movies/src/components/Navbar.js
@@ -6,11 +6,10 @@ function NavBar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">MovieDB</Navbar.Brand>
+        <Navbar.Brand href="/">MovieDB</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/tvshows">Tv Shows</Nav.Link>
             <Nav.Link href="/person">Person</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
