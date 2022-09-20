ee76ccf13014e5b19b82409eb6ce493a598bf9fa
André <a.carvalho@runtime-revolution.com>
2022-09-20 10:08:52 +0100

    type(feature): add search and show only 5 elements

--- react-movies/src/App.js
+++ react-movies/src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import Movie from './components/Movie';
 import React, { useState, useEffect } from 'react';
 
-const API_KEY = process.env.REACT_APP_API_KEY
-const API_POPULAR_MOVIES = "https://api.themoviedb.org/3/movie/popular?api_key=" + API_KEY + "&language=en-US"
-const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + "&language=en-US&include_adult=false"
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_POPULAR_MOVIES = "https://api.themoviedb.org/3/movie/popular?api_key=" + API_KEY + "&language=en-US";
+const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + "&language=en-US&include_adult=false&query=";
 
 function App() {
   // default there are no movies
@@ -16,18 +16,14 @@ function App() {
   useEffect(() => {
     fetch(API_POPULAR_MOVIES)
       .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.results);
-      });
+      .then((data) => { setMovies(data.results); });
   }, [])
 
   const actionOnSubmit = (e) => {
     e.preventDefault();
     fetch(API_SEARCH + searchVal)
       .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.results);
-      });
+      .then((data) => { setMovies(data.results); });
     setSearchVal("");
   }
 
@@ -38,15 +34,12 @@ function App() {
   return (<>
     <header>
       <form onSubmit={actionOnSubmit}>
-        <input className='search' type="search"
-          placeholder="Movie name..." value={searchVal}
-          onchange={actionOnChange} />
+        <input className='search' type="search" placeholder="Movie name..." value={searchVal}
+          onChange={actionOnChange} />
       </form>
     </header>
     <div className='movie-list'>
-      {movies.length > 0 &&
-        movies.map((movie) =>
-          <Movie key={movie.id} {...movie} />)}
+      {movies.length > 0 && movies.slice(0, 5).map((movie) => <Movie key={movie.id} {...movie} />)}
     </div>
   </>);
 }
